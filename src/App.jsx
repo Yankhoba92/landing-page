37d4dbefc6b0733779139dd89b9ddd76dfe86dee
Yankhoba <bagayoko.yan@gmail.com>
2023-11-14 16:09:26 +0100

    🚧 decouverte material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Box, Button, Stack, Typography } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SendIcon from "@mui/icons-material/Send";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    
+
+<Typography variant="subtitle1" textTransform={"uppercase"}> Caractéristique</Typography>
+<Typography variant="h1" fontWeight={600}>Plus de puissance pour plus d'efficacité</Typography>
+<Stack direction="row" spacing={2} paddingTop={5}>
+  <Box sx={{width:"50%", height:450, borderRadius:1, bgcolor:"primary.main"}}></Box>
+<Stack width={"50%"} spacing={2}>
+  <Box sx={{width:"100%", height:"50%", borderRadius:1, bgcolor:"primary.main"}}></Box>
+  <Box sx={{width:"100%", height:"50%", borderRadius:1, bgcolor:"primary.main"}}></Box>
+</Stack >
+</Stack>
+
+
+
+
+
+      {/* <div>
+        <Button variant="text" color="error" size="small">
+          text
+        </Button>
+        <br />
+        <Button variant="outlined" color="secondary" size="large">
+          Outlined
+        </Button>
+        <br />
+        <Button variant="contained" color="success" size="medium" startIcon={<SendIcon />}>
+          Contained
+        </Button>
+      </div> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
