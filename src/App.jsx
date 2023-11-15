d39cf870c30c0c3b52cc9553cd20a88384bb9bd6
Yankhoba <bagayoko.yan@gmail.com>
2023-11-15 11:19:35 +0100

    💄 modification des styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import "./App.css";
 import { Box, Stack, Typography, Fab } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
+import { useState } from "react";
 
 function App() {
+
+  const [templateArea, setTemplateArea] = useState({gridTemplateAreas: `'box1 box2' 'box1 box3'`})
+  
   return (
     <>
       <Typography variant="subtitle1" textTransform={"uppercase"}>
@@ -11,27 +15,37 @@ function App() {
       <Typography variant="h3" fontWeight={600}>
         Plus de puissance pour plus d'efficacité
       </Typography>
-      <Stack direction="row" spacing={2} paddingTop={5}>
-        <Box sx={{ width: '50%', height: 450, borderRadius: '2vw 4vw', bgcolor: "info.main", position: 'relative'}}>
-        
-          <Fab color="info.light" aria-label="add" sx={{ position: 'absolute', bottom: '5%', right: '5%', }}>
+      
+      <Stack className="box-container" marginTop={5} style={templateArea}>
+        {/* Box 1 */}
+        <Box className="box1" sx={{ bgcolor: "info.main", position: 'relative'}}>
+          <Fab 
+            aria-label="add" 
+            data-box="1"
+            sx={{ position: 'absolute', bottom: '5%', right: '5%', }}>
             <AddIcon />
           </Fab>
         </Box>
-        <Stack width="50%" spacing={2}>
-          <Box sx={{ width: '100%', height: '50%', borderRadius: '2vw', bgcolor: "secondary.main", position: 'relative'}}>
-            
-            <Fab color="info.light" aria-label="add" sx={{ position: 'absolute', bottom: '5%', right: '5%', }}>
-              <AddIcon />
-            </Fab>
+
+        {/* Box 2 */}
+        <Box className="box2" sx={{ bgcolor: "secondary.main", position: 'relative'}}>
+          <Fab 
+            aria-label="add"
+            data-box="2" 
+            sx={{ position: 'absolute', bottom: '5%', right: '5%', }}>
+            <AddIcon />
+          </Fab>
           </Box>
-          <Box sx={{ width: '100%', height: '50%', borderRadius: '2vw', bgcolor: "success.main", position: 'relative'}}>
-            
-            <Fab color="info.light" aria-label="add" sx={{ position: 'absolute', bottom: '5%', right: '5%', }}>
-              <AddIcon />
-            </Fab>
-          </Box>
-        </Stack>
+
+        {/* Box 3 */}
+        <Box className="box3" sx={{ bgcolor: "success.main", position: 'relative'}}>
+          <Fab 
+            aria-label="add"
+            data-box="3" 
+            sx={{ position: 'absolute', bottom: '5%', right: '5%', }}>
+            <AddIcon />
+          </Fab>
+        </Box>
       </Stack>
     </>
   );
